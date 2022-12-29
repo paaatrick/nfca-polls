da8e6a8601f91b2762e83fa1bede7034e3129acc
Patrick Kalita <patrick.kalita@gmail.com>
2022-12-29 09:40:32 -0800

    App title text smaller on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import { Header as Header_, Text, MediaQuery, Burger, useMantineTheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks'
 
 
 export default function Header({ opened, onToggle }) {
   const theme = useMantineTheme();
+  const isSmall = useMediaQuery(`(min-width: ${theme.breakpoints.sm}px)`)
   return (
-    <Header_ height={{ base: 70}} p="md">
+    <Header_ height={{ base: 50, sm: 70 }} p="md">
       <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
         <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
           <Burger
@@ -15,7 +17,7 @@ export default function Header({ opened, onToggle }) {
             mr="xl"
           />
         </MediaQuery>
-        <Text size='xl'>NFCA NCAA Division 1 Softball Polls</Text>
+        <Text size={isSmall ? 'xl' : 'md'}>NFCA NCAA Division 1 Softball Polls</Text>
       </div>
     </Header_>
   )
